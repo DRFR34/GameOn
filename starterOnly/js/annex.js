3cbe9432b34d99e12b16a190aa2eaa381ae64d4b
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2023-09-11 10:40:58 +0200

    Create annex.js
    WIP  -  tests de codes

---
+++ starterOnly/js/annex.js
@@ -0,0 +1,57 @@
+
+
+
+
+
+
+
+//******************************** 
+// Récupération des boutons radio
+//******************************** 
+
+// const locationButtonsList = document.querySelectorAll('input[name = "location"]');
+//         console.log("locationButtonsList :" + locationButtonsList);
+//         console.log(document.querySelectorAll('input[name = "location"]'));
+// let locationBtn = "";
+// for (let i = 0 ; i< locationButtonsList.length; i++) {
+//     if (locationButtonsList[i].checked) {
+//         locationBtn = locationButtonsList[i].value;
+//        console.log("locationBtn :" + locationBtn.value)
+//     }
+// }
+
+
+//******************************** 
+// Récupération case à cocher
+//******************************** 
+//  let checkboxCondition1 = document.getElementById('checkboxCondition1');
+//  let checkboxCondition1Status = checkboxCondition1.checked;
+//  console.log("checkboxCondition1Status :" +  checkboxCondition1Status);
+//  checkboxCondition1.addEventListener('click', ()=> {
+//     console.log("checkboxCondition1Status :" +  checkboxCondition1Status);
+//     console.log("checkboxCondition1 est coché" );
+//     console.log(checkboxCondition1.checked)
+//  })
+
+
+
+ //******************************** 
+// Récupération des imput required
+//******************************** 
+// let requiredFieldsList = form.querySelectorAll("input[required]"); // retourne la NodeList de tous les inputs requis
+//     console.log( "requiredFieldsList : " + requiredFieldsList); // node list
+// let AllFieldsAreValid = true; // valeur true si tous les champs sont valides
+//     console.log( "AllFieldsAreValid : " + AllFieldsAreValid)
+
+
+// Déclaration d'une variable booléenne  de vérification des inputs requis (sauf boutons radio) → Boolean.
+
+
+
+
+// créer une collection de tous les champs requis  → Nodelist
+// let requiredInputsList = document.querySelectorAll('input[required]');
+//     console.log("liste des inputs required : ");
+//     console.log(requiredInputsList);
+
+
